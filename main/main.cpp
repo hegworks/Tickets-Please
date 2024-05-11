72837cbf23fb8ad6b922fe2127b1c7638b4d5f71
Hessam Ghadimi <hess.gha@gmail.com>
2024-05-11 19:32:59 +0200

    window setup

--- main/main.cpp
+++ main/main.cpp
@@ -2,9 +2,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(360, 640), "Tickets, Please", sf::Style::Close | sf::Style::Titlebar);
 
     while (window.isOpen())
     {
@@ -15,8 +13,7 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color(115,115,140));
         window.display();
     }
 
