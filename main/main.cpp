cf501904db7f39c1fcf33220a973c4d7ed4d50af
Hessam Ghadimi <hess.gha@gmail.com>
2024-05-13 16:09:23 +0200

    delete extra codes from main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -13,91 +13,9 @@
 
 int main()
 {
-	srand(time(0));
-
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Tickets, Please", sf::Style::Close | sf::Style::Titlebar);
 
-
-	const int FIXED_Y_OFFSET = -50;
-	const int BETWEEN_CARDS_Y_OFFSET = 10;
-	const int CARDS_BUTTONS_Y_OFFSET = 20;
-	const int BETWEEN_BUTTONS_X_OFFSET = 10;
-
-	// =====================================
-	/*sf::Texture idCardTexture;
-	if (!idCardTexture.loadFromFile("Assets/2D/IdCard.png"))
-	{
-		std::cout << "IdCard.png doesn't exist";
-	}
-
-	sf::Sprite idCardsprite;
-	idCardsprite.setTexture(idCardTexture);
-	idCardsprite.setOrigin(idCardTexture.getSize().x / 2, idCardTexture.getSize().y / 2);
-	int idCardPosY = ProjectSettings::WindowSize::Y / 2 - idCardTexture.getSize().y / 2 + FIXED_Y_OFFSET;
-	idCardsprite.setPosition(ProjectSettings::WindowSize::X / 2, idCardPosY);*/
-	// =====================================
-
-
-
-
-
-	// =====================================
-	/*sf::Texture ticketTexture;
-	if (!ticketTexture.loadFromFile("Assets/2D/Ticket.png"))
-	{
-		std::cout << "Ticket.png doesn't exist";
-	}
-
-	sf::Sprite ticketSprite;
-	ticketSprite.setTexture(ticketTexture);
-	ticketSprite.setOrigin(ticketTexture.getSize().x / 2, ticketTexture.getSize().y / 2);
-	int ticketPosY = idCardPosY = ProjectSettings::WindowSize::Y / 2 + ticketTexture.getSize().y / 2 + FIXED_Y_OFFSET + BETWEEN_CARDS_Y_OFFSET;
-	ticketSprite.setPosition(ProjectSettings::WindowSize::X / 2, ticketPosY);*/
-	// =====================================
-
-
-
-
-	// =====================================
-	/*sf::Texture collectTicketTexture;
-	if (!collectTicketTexture.loadFromFile("Assets/2D/CollectTicket.png"))
-	{
-		std::cout << "CollectTicket.png doesn't exist";
-	}
-
-	sf::Sprite collectTicketSprite;
-	collectTicketSprite.setTexture(collectTicketTexture);
-	collectTicketSprite.setOrigin(collectTicketTexture.getSize().x / 2, collectTicketTexture.getSize().y / 2);
-	collectTicketSprite.setPosition(ProjectSettings::WindowSize::X / 2 + collectTicketTexture.getSize().x / 2 + BETWEEN_BUTTONS_X_OFFSET,
-		ticketPosY + ticketTexture.getSize().y / 2 + collectTicketTexture.getSize().y / 2 + CARDS_BUTTONS_Y_OFFSET);*/
-		// =====================================
-
-
-
-
-		// =====================================
-		/*sf::Texture collectMoneyTexture;
-		if (!collectMoneyTexture.loadFromFile("Assets/2D/CollectMoney.png"))
-		{
-			std::cout << "CollectMoney.png doesn't exist";
-		}
-
-		sf::Sprite collectMoneySprite;
-		collectMoneySprite.setTexture(collectMoneyTexture);
-		collectMoneySprite.setOrigin(collectMoneyTexture.getSize().x / 2, collectMoneyTexture.getSize().y / 2);
-		collectMoneySprite.setPosition(ProjectSettings::WindowSize::X / 2 - collectMoneyTexture.getSize().x / 2 - BETWEEN_BUTTONS_X_OFFSET,
-			ticketPosY + ticketTexture.getSize().y / 2 + collectMoneyTexture.getSize().y / 2 + CARDS_BUTTONS_Y_OFFSET);*/
-			// =====================================
-
-
-
-
-	// =====================================
-	/*sf::Text scoreText;
-	scoreText.setFont(font);
-	scoreText.setString("Score: ");
-	scoreText.setPosition(ProjectSettings::WindowSize::X / 2 - ticketTexture.getSize().x / 2, 10);*/
-	// =====================================
+	srand(time(0));
 
 	Font::LoadAssets();
 
@@ -131,17 +49,8 @@ int main()
 		}
 
 		window.clear(sf::Color(115, 115, 140));
-		/*window.draw(scoreText);
-		window.draw(idCardsprite);
-		window.draw(ticketSprite);
-		window.draw(collectTicketSprite);
-		window.draw(collectMoneySprite);*/
 		id.Draw(window);
 		ticket.Draw(window);
-		/*window.draw(IdPicturesDb::maleSprites[0]);
-		window.draw(IdPicturesDb::maleSprites[1]);
-		window.draw(IdPicturesDb::femaleSprites[2]);*/
-
 		window.display();
 	}
 
